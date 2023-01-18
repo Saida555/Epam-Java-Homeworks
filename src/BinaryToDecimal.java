bd638a5a5be0b2ef5d149b52114a0a0728fce4ce
Saida555 <ohanjanyan.saida@gmail.com>
2023-01-18 17:08:02 +0400

    Add BinaryToDecimal.java file.

---
+++ src/BinaryToDecimal.java
@@ -0,0 +1,12 @@
+public class BinaryToDecimal {
+    public static void main(String[] args) {
+        int[] arr = {1,1,0,1, 1, 1, 1, 0, 1, 0, 1, 1, 1};
+        int decimalNumber = 0;
+        int counter = 1;
+        for (int i = 0; i < arr.length; i++) {
+            decimalNumber = decimalNumber + arr[i] * counter;
+            counter = counter * 2;
+        }
+        System.out.println("Decimal number is " + decimalNumber);
+    }
+}
