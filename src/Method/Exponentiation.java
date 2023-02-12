4ea9c3ffde2268b90f4996158010bbad7459b582
Saida555 <ohanjanyan.saida@gmail.com>
2023-02-12 20:14:15 +0400

    Update Exponentiation.java file.

--- src/Method/Exponentiation.java
+++ src/Method/Exponentiation.java
@@ -2,14 +2,14 @@ package Method;
 
 public class Exponentiation {
     public static void main(String[] args) {
-        System.out.println(exponent(0));
+        System.out.println(exponent(5));
         System.out.println(exponent(5, 2));
     }
 
     public static int exponent(int number) {
         int result = 1;
         for (int i = 0; i < number; i++) {
-            result = result * number;
+            result *= number;
         }
         return result;
     }
@@ -17,7 +17,7 @@ public class Exponentiation {
     public static int exponent(int number, int power) {
         int result = 1;
         for (int i = 0; i < power; i++) {
-            result = result * number;
+            result *= number;
         }
         return result;
     }
